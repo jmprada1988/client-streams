5bb9c4a1910556bf0a0466ecc6291cf86a15819b
davirlein1988 <lein.davir@gmail.com>
2019-09-23 17:48:23 -0500

    finished reducers and actions for streams crud

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-    return ( <div>Stream list</div> );
+
+class StreamList extends Component {
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+    render() { 
+        return ( <div>Stream list</div> );
+    }
 }
  
-export default StreamList;
+export default connect(null, { fetchStreams })(StreamList);
