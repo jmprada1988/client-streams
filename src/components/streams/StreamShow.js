bcbc923429e4f6dcaf642e2e6f9a2318952218f1
davirlein1988 <lein.davir@gmail.com>
2019-10-12 22:17:18 -0500

    Connected application to stream from and OBS stream

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,17 +1,49 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux';
 import { fetchstream } from '../../actions/index';
+import flv from 'flv.js'
 
 class StreamShow extends Component {
-
+    constructor(props){
+        super(props)
+        this.videoRef = React.createRef()
+    }
     componentDidMount() {
-        this.props.fetchstream(this.props.match.params.id)
+        const { id } = this.props.match.params
+        this.props.fetchstream(id)
+        this.createPlayer()
+    }
+    componentDidUpdate() {
+        this.createPlayer()
+    }
+    componentWillUnmount() {
+        this.player.destroy()
+    }
+    
+    
+    createPlayer() {
+        if (this.player || !this.props.stream) {
+            return
+        }
+        const { id } = this.props.match.params
+        
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        })
+        this.player.attachMediaElement(this.videoRef.current)
+        this.player.load()
     }
     render() { 
         const { stream } = this.props
         if(!stream) return <div>Loading...</div>
         return (
             <div>
+                <video 
+                ref={this.videoRef} 
+                style={{width: '100%'}}
+                controls
+                />
                 <h1>
                     {stream.title}
                 </h1>
